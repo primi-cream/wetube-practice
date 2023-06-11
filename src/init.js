5717cf1b111e98651f6e6f6ffbfb9e4cea4cb468
최연욱 <drclockd@gmail.com>
2023-06-11 10:06:06 +0900

    Added comments

--- src/init.js
+++ src/init.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import "./db";
 import "./models/Video";
 import "./models/User";
+import "./models/Comment";
 import app from "./app";
 
 app.listen(process.env.PORT,() => {console.log(`Start Server http://localhost:${process.env.PORT}`)});
